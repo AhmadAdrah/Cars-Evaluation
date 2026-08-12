3ada84f6e1b2e166ff6d92d6dc753972a1844dda
@ahmadadrah4 <ahmadadrah4@gmail.com>
2026-08-12 15:24:26 +0300

    Add CORS headers support to settings

--- core/settings.py
+++ core/settings.py
@@ -45,9 +45,15 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'apps.accounts',
+    'corsheaders',
 ]
 
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
